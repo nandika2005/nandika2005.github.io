fe0845f0c238f59c0fe700cde6b9901f5d022e4d
nandika2005 <145485411+nandika2005@users.noreply.github.com>
2023-10-18 00:16:39 +0530

    update

---
+++ module4sol/script1.js
@@ -0,0 +1,23 @@
+var names=new Array();
+ names[0]="Yaakov";
+ names[1]="kate";
+ names[2]="dom";
+ names[3]="Jihd";
+ names[4]="deeij";
+ names[5]="geri";
+ names[6]="jeit";
+ names[7]="pire";
+ names[8]="ajum";
+ names[9]="efcm";
+ names[10]="vhfkg";
+ names[11]="hyrb";
+ names[12]="jfybhb";
+ names[13]="jbjjbj";
+ for(var i=0;i<names.length;i++){
+ 	if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j'){
+ console.log("Goodbye " +names[i]);
+ }
+ else{
+ 	console.log("Hello "+names[i]);
+ }
+}
